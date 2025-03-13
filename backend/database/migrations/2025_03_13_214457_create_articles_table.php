ff74de6b49148e59abc3b6eef7d4168e546fb50d
Evan Vink <evink2@my.bcit.ca>
2025-03-13 15:14:48 -0700

    added models and controllers for user and article

---
+++ backend/database/migrations/2025_03_13_214457_create_articles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->integer("ArticleId");
+            $table->string('Title');
+            $table->string('Body');
+            $table->datetime('CreateDate');
+            $table->date('StartDate');
+            $table->date('EndDate');
+            $table->string('ContributorUsername');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
